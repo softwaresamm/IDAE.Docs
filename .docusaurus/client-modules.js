fe9a467ab55007a1e0d1573456345eddc6b968ca
Jeffrey Caro Roncancio <jcaror.jcr@gmail.com>
2025-12-11 11:58:50 -0500

    initial commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("C:\\idaeSoluciones\\IDAE.Docs\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
+  require("C:\\idaeSoluciones\\IDAE.Docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\idaeSoluciones\\IDAE.Docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\idaeSoluciones\\IDAE.Docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\idaeSoluciones\\IDAE.Docs\\src\\css\\custom.css"),
+];
